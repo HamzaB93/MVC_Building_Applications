d4bbc1e3fd04c8b3b65238c5d35474c5655d7704
HamzaB93 <hamzabhatti93@gmail.com>
2018-07-23 17:18:39 +0100

    Created MVC site. Used view bag and strongly typed model to display information on About View

---
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            // Add information to the view bag so that the view can use it
+            //ViewBag.Message = "Your app description page.";
+
+            // Creating model object and send the properties to the View
+            var model = new AboutModel();
+            model.Name = "Hamza";
+            model.Location = "London, UK";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
