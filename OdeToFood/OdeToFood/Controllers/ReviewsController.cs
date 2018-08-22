375c580389794b9d4294f870074273576f54bb29
HamzaB93 <hamzabhatti93@gmail.com>
2018-08-22 21:34:05 +0100

    Added Annotations to code for validation and display

--- OdeToFood/OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/OdeToFood/Controllers/ReviewsController.cs
@@ -66,7 +66,7 @@ namespace OdeToFood.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit([Bind(Exclude="ReviewerName")]RestaurantReview review)
+        public ActionResult Edit(RestaurantReview review)
         {
             if (ModelState.IsValid)
             {
