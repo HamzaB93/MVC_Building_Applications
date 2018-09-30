9237bd4d49f0ac7dd5e665969bb3f6c19c5beffe
HamzaB93 <hamzabhatti93@gmail.com>
2018-09-30 20:50:37 +0100

    Added jquery to manually make asynch call for search. Search works but doesnt keep position on screen

---
+++ OdeToFood/OdeToFood/Scripts/otf.js
@@ -0,0 +1,34 @@
+﻿
+$(function () {
+    
+    
+    var ajaxFormSubmit = function () {
+        //grab reference to the from and wwrap jquery around it 
+        var $form = $(this);
+
+        //create options object
+        var options = {
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        };
+
+        //Make asynch call
+        // Use ajax, more flexible, when done call the function and save result in data
+        $.ajax(options).done(function (data) {
+
+            // what dom element to update with the data
+            var $target = $($form.attr("data-otf-target"));
+            
+            //replace the target with the html we got back
+            $target.replaceWith(data);
+
+            // Prevent browser from completed default action
+            return false;
+        });
+    };
+
+    // Use jquery to find a data attribute based on name and what they are set to
+    // Then wire it to an event and call our own function
+    $("form[data-otf-ajax='true']").submit(ajaxFormSubmit);
+});
